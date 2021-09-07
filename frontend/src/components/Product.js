3f2d3658d63d7eba29f023916a86ec4662950d44
IbrahimG88 <goharyibrahim@gmail.com>
2021-09-07 15:41:32 +0200

    Implement React Router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Card, ListGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <ListGroup variant='flush'>
           <ListGroup.Item>{product.category}</ListGroup.Item>
