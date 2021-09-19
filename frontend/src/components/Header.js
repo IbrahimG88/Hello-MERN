602fa11643b76b51a836fb6a9d6b6b9b0f967632
IbrahimG88 <goharyibrahim@gmail.com>
2021-09-20 00:22:53 +0200

    Added empty cart component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,17 +7,16 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>Inventory Manager</Navbar.Brand>
-          </LinkContainer>
+          <Navbar.Brand href='/'>ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i> Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
